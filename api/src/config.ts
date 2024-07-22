2ec6fcd39e549e8f3d4ff7cace4dbb3a3207d777
Alexey Sokolov <alexvyber@gmail.com>
2024-07-22 14:46:34 +0300

    api setup

--- api/src/config.ts
+++ api/src/config.ts
@@ -7,9 +7,7 @@ type Config = {
   version: string
   port: number
 
-  ipAddress: {
-    ip: string
-  }
+  ipAddress: { ip: string }
 
   rtcMinPort: number
   rtcMaxPort: number
@@ -23,12 +21,10 @@ export const config = {
   version: "0.0.0",
   port: Number.parseInt(process.env.API_PORT || "3000", 10),
 
-  ipAddress: {
-    ip: process.env.API_PUBLIC_IP_ADDRESS!,
-  },
+  ipAddress: { ip: process.env.API_PUBLIC_IP_ADDRESS! },
 
-  rtcMinPort: Number.parseInt(process.env.RTC_MIN_PORT || `${2 ** 13}`, 10),
-  rtcMaxPort: Number.parseInt(process.env.RTC_MAX_PORT || `${2 ** 15}`, 10),
+  rtcMinPort: Number.parseInt(process.env.API_RTC_MIN_PORT || `${2 ** 13}`, 10),
+  rtcMaxPort: Number.parseInt(process.env.API_RTC_MAX_PORT || `${2 ** 15}`, 10),
 
   mediaCodecs: [
     { kind: "audio", mimeType: "audio/opus", clockRate: 48000, channels: 2 },
@@ -40,7 +36,7 @@ export const config = {
     },
   ],
 
-  mediasoupLogLevel: "debug",
+  mediasoupLogLevel: process.env.API_MEDIASOUP_LOG_LEVEL as WorkerLogLevel,
 } satisfies Config
 
 // Stupidly simple env validation doesn't require to install library
@@ -49,6 +45,7 @@ async function validateEnv() {
     required("API_PORT")
     integer("API_PORT")
     required("API_PUBLIC_IP_ADDRESS")
+    ENUM("API_MEDIASOUP_LOG_LEVEL", ["debug", "error", "none", "warn"])
   } catch (error) {
     logger.error(error)
 
@@ -70,5 +67,13 @@ function integer(name: string) {
     isNaN(parseInt(process.env?.[name]))
   ) {
     throw new Error(`${name} env variable must be of type integer`)
+  }
+}
+
+function ENUM(name: string, entries: string[]) {
+  required(name)
+
+  if (!entries.includes(process.env?.[name]!)) {
+    throw new Error(`${name} env variable must be one of ${entries.map((w) => `'${w}'`).join(", ")}`)
   }
 }
