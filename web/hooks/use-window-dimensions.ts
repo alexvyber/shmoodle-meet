f811b419e362cb1acf1737f0488955b81763cf97
Alexey Sokolov <alexvyber@gmail.com>
2024-07-22 14:45:26 +0300

    useDeminsions

---
+++ web/hooks/use-window-dimensions.ts
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+type WindowDimentions = {
+  width: number | undefined
+  height: number | undefined
+}
+
+export function useWindowDimensions(): WindowDimentions {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
+    width: undefined,
+    height: undefined,
+  })
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({ width: window.innerWidth, height: window.innerHeight })
+    }
+
+    handleResize()
+
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, []) // Empty array ensures that effect is only run on mount
+
+  return windowDimensions
+}
